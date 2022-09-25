7694e71ea8517210b43161de08802ae77a21abe6
Frank Tineo <frankfelix2016@outlook.es>
2022-09-25 16:44:57 -0400

    Segundo commit

---
+++ assets/js/alerts.js
@@ -0,0 +1,6 @@
+// alert("HolaMundo");
+// let nombre = prompt("Cual es tu nombre?");
+// console.log(nombre);
+// let select = confirm("Esta seguro de borrar esto?");
+// console.log(select);
+console.log(global);
---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+// console.log("Hola mundo");
+// alert("Hola desde app.js");
+
+console.log(miNombreNuevo);
+let a = 10,
+  b = 10,
+  c = "Hola ",
+  d = "Spiderman",
+  x = a + b;
+const saludo = c + d;
+
+// console.log({ x });
+// console.log({ a });
+// console.log({ c });
+// console.log({ d });
+
+// console.table({ a, b, c, d, x });
+
+c = "Hola de nuevo";
+
+var miNombre = "Frank";
+
+let miNombreNuevo = "Frank";
