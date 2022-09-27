7c5f9ee4fdc53086b27af5a283eeb283b3622029
Frank Tineo <frankfelix2016@outlook.es>
2022-09-26 21:21:04 -0400

    clase 20 terminada

---
+++ assets/js/primitivos.js
@@ -0,0 +1,31 @@
+let nombre = "frank Feliz";
+console.log(nombre);
+
+nombre = "Francisco";
+console.log(nombre);
+
+nombre = "tie mey";
+console.log(nombre);
+console.log(typeof nombre);
+
+nombre = 123;
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+let edad1 = 33.00001;
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(typeof soyNull);
+
+let symbol1 = Symbol("a");
+let symbol2 = Symbol("a");
+console.log(typeof symbol1);
