054d19ca6a1f29a0310a90893194ba5a20868589
Frank Tineo <frankfelix2016@outlook.es>
2022-09-26 21:58:42 -0400

    clase 22 terminada

---
+++ assets/js/arreglos.js
@@ -0,0 +1,20 @@
+// const arr = new Array(10);
+
+// const arr = [];
+
+let videoJuegos = ["Mario 3", "Megaman", "Fortnite"];
+console.log(videoJuegos);
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+  true,
+  123,
+  "Frank",
+  1 + 2,
+  function () {},
+  () => {},
+  { a: 1 },
+  ["x", "Megaman", "Cero", ["Dr. Willy", "Woodman"]],
+];
+console.log(arregloCosas[0]);
+console.log(arregloCosas[7][3][1]);
